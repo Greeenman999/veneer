6e4414a923395db606428715aef642febd9675d3
Greenman999 <git@greenman999.de>
2026-01-13 22:42:05 +0100

    feat(examplemod): update test method to open LayrScreen and render content

--- src/main/java/de/greenman999/Test.java
+++ src/main/java/de/greenman999/Test.java
@@ -1,9 +1,18 @@
 package de.greenman999;
 
 import de.greenman999.layr.api.LayrProvider;
+import de.greenman999.layr.api.gui.screen.LayrScreen;
+import de.greenman999.layr.api.renderer.RenderContext;
+
+import java.awt.*;
 
 public class Test {
     public static void test() {
-        LayrProvider.get().init();
+        LayrProvider.get().getScreenManager().open(new LayrScreen() {
+            @Override
+            public void renderContent(RenderContext ctx, int mouseX, int mouseY, float delta) {
+                ctx.fillRect(10, 10, 100, 100, Color.WHITE);
+            }
+        });
     }
 }
