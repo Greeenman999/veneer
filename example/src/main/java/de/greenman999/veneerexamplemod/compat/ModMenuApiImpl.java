5d0e53f7a268ffc0c581fefec6dd43fc3b1350d9
Greenman999 <git@greenman999.de>
2026-01-18 02:47:12 +0100

    feat: update ModMenuApiImpl to use LayrScreenWrapper for config screen

--- example/src/main/java/de/greenman999/veneerexamplemod/compat/ModMenuApiImpl.java
+++ example/src/main/java/de/greenman999/veneerexamplemod/compat/ModMenuApiImpl.java
@@ -2,20 +2,13 @@ package de.greenman999.veneerexamplemod.compat;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
-import de.greenman999.Test;
-import net.minecraft.client.gui.screens.Screen;
-import net.minecraft.network.chat.Component;
+import de.greenman999.layr.impl.gui.screen.LayrScreenWrapper;
+import de.greenman999.veneerexamplemod.TestScreen;
 
 public class ModMenuApiImpl implements ModMenuApi {
 
     @Override
     public ConfigScreenFactory<?> getModConfigScreenFactory() {
-        return (parent) -> new Screen(Component.empty()) {
-            @Override
-            protected void init() {
-                super.init();
-                Test.test();
-            }
-        };
+        return (parent) -> new LayrScreenWrapper(new TestScreen(), parent);
     }
 }
